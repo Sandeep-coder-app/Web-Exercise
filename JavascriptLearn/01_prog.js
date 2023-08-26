9f2446ab0b4a07d8e8e8f8b14180bc58d1f93565
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-08-26 14:52:05 +0530

    js

---
+++ JavascriptLearn/01_prog.js
@@ -0,0 +1,9 @@
+var x = typeof("Akash");
+var y = typeof("Akash");
+
+if(x == y) 
+{
+    console.log("Hello, World");
+} else {
+    console.log("Bye, Bye");
+}
---
+++ JavascriptLearn/02_odd_even.js
@@ -0,0 +1,8 @@
+//ODD AND EVEN
+let num = 9;
+
+if(num%2==0) {
+    console.log("EVEN...");
+} else {
+    console.log("ODD...");
+}
