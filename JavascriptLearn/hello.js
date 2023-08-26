8e8c148cc809a04013af24af1ffbd27d194f37ad
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-08-26 13:08:37 +0530

    js

---
+++ JavascriptLearn/hello.js
@@ -0,0 +1,5 @@
+let text = "Hello, World";
+console.log(text);
+
+var text1 = "Hello, Hi";
+console.log(text1);
---
+++ JavascriptLearn/index.js
@@ -0,0 +1,24 @@
+let x = 5;
+let y = 10;
+let z = x + y;
+
+console.log("Add: ",z);
+
+let a = 10 - 15;
+console.log("Sub: ",a);
+
+let b = 10 * 2;
+console.log("Mul: ",b);
+
+let c = 10 / 5;
+console.log("Div: ",c);
+
+let d = 5 % 500;
+console.log("Modulus: ",d);
+
+var e = 10;
+e++;
+console.log("Increment: ",e);
+
+e++;
+console.log(e);
