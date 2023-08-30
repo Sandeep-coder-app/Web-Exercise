a197745c2f056d7f208589a9d6f577d44bd376ac
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-08-30 23:10:31 +0530

    variable

---
+++ JS/02_variable.js
@@ -0,0 +1,20 @@
+const accountId = 123456;
+let accountEmail = "Sandeep@gmail.com"
+var accountPassword = "5623"
+accountCity = "Kanpur"
+let accountState
+//accountId = 12454
+
+console.log(accountId);
+
+accountEmail = "San@gmail.com"
+accountPassword = 45678
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/* 
+Prefer not use var
+because of issue in block scope and functional scope
+*/
+
