dbb55eda3a810f1fd791459f26f46b3e5eb51cc7
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-09-01 17:24:54 +0530

    datatype

---
+++ JS/03_datatype.js
@@ -0,0 +1,27 @@
+let name = "Sandeep Singh";
+let id = 1234;
+let isLoggnedIn = true;
+
+//number
+//bigInt
+//string => ""
+//boolean => true / false
+// null => standalone value
+//undefined
+//symbol => unique
+
+// PDT
+//object
+
+console.log(typeof undefined)
+
+console.log(typeof null);
+
+console.log(typeof id);
+
+
+
+
+
+
+
