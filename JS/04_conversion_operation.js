ecf11b8fd7d9cd840da3c1cb041c37e182c1f4d2
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-09-02 18:02:54 +0530

    conversion

--- JS/04_conversion_operation.js
+++ JS/04_conversion_operation.js
@@ -1,54 +1,101 @@
-let score = "33"
-let score1 = "33abc"
-let score2 = null
-let score3 = undefined
+// let score = "33"
+// let score1 = "33abc"
+// let score2 = null
+// let score3 = undefined
 
 //boolean
 
-let isLoggnedIn = false
+// let isLoggnedIn = false
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-let valueInNumber1 = Number(score1)
+// let valueInNumber1 = Number(score1)
 
-let valueInNumber2 = Number(score2)
+// let valueInNumber2 = Number(score2)
 
-let valueInNumber3 = Number(score3)
+// let valueInNumber3 = Number(score3)
 
 
-let valueInNumber4 = Number(isLoggnedIn)
+// let valueInNumber4 = Number(isLoggnedIn)
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber1); //NaN
+//console.log(valueInNumber1); //NaN
 
-console.log(valueInNumber2); //0
+//console.log(valueInNumber2); //0
 
-console.log(valueInNumber3); //underfined
+//console.log(valueInNumber3); //underfined
 
-console.log(valueInNumber4);//is value true it give 1 or is value false it give 0
+//console.log(valueInNumber4);//is value true it give 1 or is value false it give 0
 
 //"33" => 33
 //"33abc" => NaN
 // true => 1; false => 0
 
-let isLoggnedIn1 = "Sandeep"
+// let isLoggnedIn1 = "Sandeep"
 
-let booleanIsLoggedIn = Boolean(isLoggnedIn1)
+// let booleanIsLoggedIn = Boolean(isLoggnedIn1)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 //"Sandeep" => true 
 
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let StringNumber = String(someNumber)
+// let StringNumber = String(someNumber)
 
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// ********** Operations **********
+
+// let str1 = "Sandeep"
+// let str2 = " Singh"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + 2);
+// console.log(1 + "2");
+// console.log((3+2) - 5 * 2 );
+// console.log(true);
+
+// console.log(+true); // 1
+// console.log(+false); // 0
+
+// console.log(+ "");
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+//Prefix
+let x = 10;
+//let y = ++x
+++x
+console.log( + " " + x);
+
+//Postfix
+
+let a = 10;
+let b = a++
+b = a++
+console.log(b);
+console.log(a);
